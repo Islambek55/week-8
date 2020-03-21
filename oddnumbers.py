c2e874941bcc2e38faa957b6b6aa47e43306031b
ABDIKARIM ISLAMBEK <47269653+Islambek55@users.noreply.github.com>
2020-03-21 09:34:32 +0600

    Update oddnumbers.py

--- oddnumbers.py
+++ oddnumbers.py
@@ -1,5 +1,15 @@
-s=input()
-a=[int(s) for s in s.split()]
-for i in a:
-    if int(i)%2 == 0:
-       print(i, end=' ')
+a = int(input("Введите А = "))
+b = int(input("Введите B = "))
+ch = 0
+nech = 0
+n = a
+while (n >= a) and (n <= b) and (a <= 100) and (b <= 100):
+    if n % 2 == 0:
+        ch = ch + n
+        n = n + 1
+    else:
+        nech = nech + n
+        n = n + 1
+
+
+print("Сумма четных = " + str(ch))
